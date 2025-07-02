d1e71e512f6aad9fd812b597bbf2ee25319a6076
Twix1288 <rishitagni@gmail.com>
2025-07-02 01:54:48 -0700

    Final

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
           content="ca-pub-3182066441920648"
         />
         {/* Core AdSense script load */}
-        <Script
+        <script
           strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3182066441920648"
           crossOrigin="anonymous"
